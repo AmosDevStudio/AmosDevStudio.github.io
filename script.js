8bac6aa97f32dae7d9d982da32a41fb1379efddf
Amos <81512320+AmosDevStudio@users.noreply.github.com>
2022-09-26 15:53:15 +0200

    home page

---
+++ script.js
@@ -0,0 +1,38 @@
+var slideIndex = 0;
+showDivs(slideIndex);
+
+const elements = document.querySelectorAll(".fade-in");
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-visible", entry.isIntersecting)
+    })
+}, {
+    threshold: 0.5
+})
+elements.forEach(element => {
+    observer.observe(element);
+})
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("indicator");
+    if (n > x.length - 1) { slideIndex = 0 }
+    if (n < 0) { slideIndex = x.length - 1 };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" white-bg", "");
+    }
+    x[slideIndex].style.display = "block";
+    dots[slideIndex].className += " white-bg";
+}
